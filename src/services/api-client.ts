b14c9ccf7fd9cf5ff45e089c3a2a654fda3b2298
David A Carrillo <71539290+DACT214@users.noreply.github.com>
2025-12-28 17:43:44 -0800

    re-build for deployment

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: import.meta.env.VITE_RAWG_API_KEY,
+    key: "e4f1705890b640ebb426c30ff7e517de",
   },
 });
